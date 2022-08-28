70ebb5f0bf5ff7fc925a980df5125ab8161ff0d5
Thomas Miller <thomas.miller147@gmail.com>
2022-08-28 21:51:37 +0200

    :sparkles: Game

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
+ */
+class GameFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->words(2, true),
+            'invite_key' => fake()->regexify('[A-Z]{5}[0-4]{1}'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Game;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(1)->create([
+            'name' => 'Meuss',
+            'email' => 'thomas.miller147@gmail.ch',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = User::factory()->count(9)->create();
+
+        $users->each(function ($item, $key) {
+            Game::factory()->for($item)->create();
+        });
     }
 }
